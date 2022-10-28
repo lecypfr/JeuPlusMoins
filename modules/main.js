9684fcc0e846fed3120c7798e41f1355cf1c414e
lecyp12 <contact@cyprien-cotinaut.com>
2022-10-28 11:21:19 +0200

    nettoyage main

--- modules/main.js
+++
@@ -1,9 +0,0 @@
-import { saveScore } from "./Scores.js";
-
-
-console.log(saveScore('antoine', 8));
-console.log(saveScore('jeremy', 4));
-console.log(saveScore('cyprien', 3));
-console.log(saveScore('ugo', 6));
-console.log(saveScore('jeremy', 2));
-console.log(saveScore('marc', 10));
