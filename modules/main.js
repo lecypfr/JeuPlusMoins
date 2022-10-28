25361991768330101ef31b7afd642556c311ced8
lecyp12 <contact@cyprien-cotinaut.com>
2022-10-28 09:42:31 +0200

    édition fonction savescore

---
+++ modules/main.js
@@ -0,0 +1,10 @@
+import { savescore } from "./savescore.js";
+
+export var scores= [];
+
+savescore('antoine', 8);
+savescore('jeremy', 4);
+savescore('cyprien', 3);
+savescore('ugo', 6);
+savescore('jeremy', 2);
+savescore('marc', 10);
