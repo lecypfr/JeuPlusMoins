8466adba52fa10e819c405413832f420abf5f9ca
lecyp12 <contact@cyprien-cotinaut.com>
2022-10-28 10:48:30 +0200

    gestion si score est un nouveau score

--- modules/savescore.js
+++ modules/savescore.js
@@ -6,12 +6,16 @@ export function savescore( username, score){
   const index = scores.findIndex(score => {
     return score.username === username;
   });
+
+  let isNewPosition = false;
   
   if (index>=0) {
     if (scores[index].score > score) {
       scores[index].score = score;
+      isNewPosition = true;
     }
   } else {
     scores.push(new Score(username, score));
   }
+  
 }
