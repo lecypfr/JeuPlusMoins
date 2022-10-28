f03676fa67ccfbbd93a44c711f0a4971805f825a
Teddy CLEMENT DELS <teddy.clement_dels@outlook.fr>
2022-10-28 11:14:57 +0200

    Création de NewGame

---
+++ modules/Newgame.js
@@ -0,0 +1,17 @@
+
+
+function NewGame(newPlayerName, newDifficultyValue){
+            let playerName = newPlayerName;
+            let difficultyValue = newDifficultyValue;
+
+            let mysteryValue =  Math.random() * 100;
+}
+
+
+export default {   
+    NewGame
+
+}
+
+
+
---
+++ modules/main.js
@@ -0,0 +1,1 @@
+import I1 from "./Newgame.js";
